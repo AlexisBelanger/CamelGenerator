ea4e634ba602b1292e0c692f33e84fed912180ea
Ivan <delgado.ivan@protonmail.com>
2019-12-10 21:44:35 +0100

    best layout

--- app/src/main/java/com/example/applicationtest/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/applicationtest/ui/gallery/GalleryFragment.java
@@ -1,32 +1,15 @@
 package com.example.applicationtest.ui.gallery;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.example.applicationtest.AcceuilActivity;
-import com.example.applicationtest.Amelioration.Balkany;
-import com.example.applicationtest.Amelioration.CodeMaster;
-import com.example.applicationtest.Amelioration.Fortnite;
-import com.example.applicationtest.Amelioration.Nord;
-import com.example.applicationtest.GameState;
-import com.example.applicationtest.LabActivity;
-import com.example.applicationtest.MainActivity;
 import com.example.applicationtest.R;
-import com.example.applicationtest.ui.home.HomeViewModel;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class GalleryFragment extends Fragment {
 
